9525147edc48965015ad9bfdc787feabb88f2b56
ARJUN_RAWAL <arjunrawal07@gmail.com>
2020-07-30 13:42:21 -0400

    restarted map is now displayed with Seattle as initialCenter

--- covidtestinglocatinos/src/App.js
+++ covidtestinglocatinos/src/App.js
@@ -1,41 +1,24 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper } from "google-maps-react";
+import MapWithASearchBox from "./searchBox.js";
 
 const mapStyles = {
   width: "100%",
   height: "100%",
 };
-
 class MapContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { userLocation: { lat: 32, lng: 32 } };
-  }
-  componentWillMount(props) {
-    this.setState({
-      userLocation: navigator.geolocation.getCurrentPosition(
-        this.renderPosition
-      ),
-    });
-  }
-  renderPosition(position) {
-    return { lat: position.coords.latitude, lng: position.coords.longitude };
-  }
-
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={14}
+        initialCenter={{ lat: 47.444, lng: -122.176 }}
         style={mapStyles}
-        initialCenter={{
-          lat: 47.444,
-          lng: -122.176,
-        }}
+        zoom={8}
       />
     );
   }
 }
+
 export default GoogleApiWrapper({
-  apiKey: "AIzaSyDn5XVYxz2sC6f4oshuE_4sqlFGLVuqIPA",
+  apiKey: "AIzaSyBixvzbtmFQ-ogW62l7UKyKZXKs3zUUq5M",
 })(MapContainer);
--- covidtestinglocatinos/src/Map.js
+++ covidtestinglocatinos/src/Map.js
@@ -1,57 +1,234 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from "react";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow,
+} from "react-google-maps";
+import Geocode from "react-geocode";
+Geocode.setApiKey("AIzaSyBixvzbtmFQ-ogW62l7UKyKZXKs3zUUq5M");
+Geocode.enableDebug();
 
-const mapStyles = {
-  map: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-  },
-};
-
-//Create component called CurrentLocation
-export class CurrentLocation extends React.Component {
+class Map extends Component {
   constructor(props) {
     super(props);
-    let { lat, lng } = this.props.initialCenter;
+    //Set state properties to empty strings
     this.state = {
-      currentLocation: {
-        lat: lat,
-        lng: lng,
+      address: "",
+      city: "",
+      state: "",
+      zipcode: "",
+      //will pass lat and long from here into map component
+      mapPosition: {
+        lat: this.props.center.lat,
+        lng: this.props.center.lng,
+      },
+      markerPosition: {
+        lat: this.props.center.lat,
+        lng: this.props.center.lng,
       },
     };
   }
-  //Define recenterMap fx which only gets called when currentLocation in component's state is updated and uses .panTo() method to chance center of map
-  recenterMap() {
-      let map = this.map;
-      let current = this.state.currentLocation;
-      let google = this.props.google;
-      let maps = google.maps;
-      if (map) {
-          let center = new maps.LatLng(current.lat, current.lng);
-          map.panTo(center);
+  /**
+   * Get the current address from the default map position and set those values in the state
+   */
+  componentDidMount() {
+    Geocode.fromLatLng(
+      this.state.mapPosition.lat,
+      this.state.mapPosition.lng
+    ).then(
+      (response) => {
+        console.log(response);
+        const address = response.results[0].formatted_address,
+          addressArray = response.results[0].address_components,
+          city = this.getCity(addressArray),
+          area = this.getArea(addressArray),
+          state = this.getState(addressArray);
+
+        console.log("city", city, area, state);
+
+        this.setState({
+          address: address ? address : "",
+          area: area ? area : "",
+          city: city ? city : "",
+          state: state ? state : "",
+        });
+      },
+      (error) => {
+        console.error(error);
       }
+    );
   }
-  //Check if map is loaded and if browser's current location is provided and recenter map to it
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
+  /**
+   * Component should only update ( meaning re-render ), when the user selects the address, or drags the pin. If shouldComponentUpdate is not used, then every time user changes input, a new API call will fire and you could reach max number of calls.
+   *
+   * @param nextProps
+   * @param nextState
+   * @return {boolean}
+   */
+  shouldComponentUpdate(nextProps, nextState) {
+    if (
+      this.state.markerPosition.lat !== this.props.center.lat ||
+      this.state.address !== nextState.address ||
+      this.state.city !== nextState.city ||
+      this.state.zipcode !== nextState.zipcode ||
+      this.state.state !== nextState.state
+    ) {
+      //If any of the above have changed, then component will update and re-render
+      return true;
+    } else if (this.props.center.lat === nextProps.center.lat) {
+      return false;
     }
-    if (prevState.currentLocation !== this.state.currentLocation) {
-      this.recenterMap();
+  }
+  /**
+   * Get the city and set the city input value to the one selected
+   *
+   * @param addressArray
+   * @return {string}
+   */
+  getCity = (addressArray) => {
+    let city = "";
+    for (let i = 0; i < addressArray.length; i++) {
+      if (
+        addressArray[i].types[0] &&
+        "administrative_area_level_2" === addressArray[i].types[0]
+      ) {
+        city = addressArray[i].long_name;
+        return city;
+      }
     }
+  };
+  /**
+   * Get the area and set the area input value to the one selected
+   *
+   * @param addressArray
+   * @return {string}
+   */
+  getArea = (addressArray) => {
+    let area = "";
+    for (let i = 0; i < addressArray.length; i++) {
+      if (addressArray[i].types[0]) {
+        for (let j = 0; j < addressArray[i].types.length; j++) {
+          if (
+            "sublocality_level_1" === addressArray[i].types[j] ||
+            "locality" === addressArray[i].types[j]
+          ) {
+            area = addressArray[i].long_name;
+            return area;
+          }
+        }
+      }
+    }
+  };
+  /**
+   * Get the address and set the address input value to the one selected
+   *
+   * @param addressArray
+   * @return {string}
+   */
+  getState = (addressArray) => {
+    let state = "";
+    for (let i = 0; i < addressArray.length; i++) {
+      for (let i = 0; i < addressArray.length; i++) {
+        if (
+          addressArray[i].types[0] &&
+          "administrative_area_level_1" === addressArray[i].types[0]
+        ) {
+          state = addressArray[i].long_name;
+          return state;
+        }
+      }
+    }
+  };
+  /**
+   * And function for city,state and address input
+   * @param event
+   */
+  onChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  /**
+   * This Event triggers when the marker window is closed
+   *
+   * @param event
+   */
+  onInfoWindowClose = (event) => {};
+  render() {
+    const AsyncMap = withScriptjs(
+      withGoogleMap((props) => (
+        <GoogleMap
+          google={this.props.google}
+          defaultZoom={this.props.zoom}
+          defaultCenter={{
+            lat: this.state.mapPosition.lat,
+            lng: this.state.mapPosition.lng,
+          }}
+        ></GoogleMap>
+      ))
+    );
+    let map;
+    if (this.props.center.lat !== undefined) {
+      map = (
+        <div>
+          <div>
+            <div className="form-group">
+              <label htmlFor="">City</label>
+              <input
+                type="text"
+                name="city"
+                className="form-control"
+                onChange={this.onChange}
+                readOnly="readOnly"
+                value={this.state.city}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">State</label>
+              <input
+                type="text"
+                name="state"
+                className="form-control"
+                onChange={this.onChange}
+                readOnly="readOnly"
+                value={this.state.state}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Address</label>
+              <input
+                type="text"
+                name="address"
+                className="form-control"
+                onChange={this.onChange}
+                readOnly="readOnly"
+                value={this.state.address}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Zipcode</label>
+              <input
+                type="text"
+                name="address"
+                className="form-control"
+                onChange={this.onChange}
+                readOnly="readOnly"
+                value={this.state.zipcode}
+              />
+            </div>
+          </div>
+          <AsyncMap
+            googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBixvzbtmFQ-ogW62l7UKyKZXKs3zUUq5M&libraries=places"
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: this.props.height }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
+        </div>
+      );
+    } else {
+      map = <div style={{ height: this.props.height }} />;
+    }
+    return map;
   }
 }
-
-export default CurrentLocation;
-
-CurrentLocation.defaultProps = {
-  zoom: 14,
-  initialCenter: {
-    lat: -1.2884,
-    lng: 36.8233,
-  },
-  //Set map with center in case current location not provided
-  centerAroundCurrentLocation: false,
-  visible: true,
-};
+export default Map;
